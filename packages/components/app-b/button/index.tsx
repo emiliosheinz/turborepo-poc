0180f86df41e50b0d0d9e5c3b0ea0217ec0b108c
Emilio Heinzmann <emiliosheinz@gmail.com>
2022-08-22 11:30:18 -0300

    refactor: :recycle: properly flavoring files

--- packages/components/app-b/button/index.tsx
+++ packages/components/app-b/button/index.tsx
@@ -3,5 +3,7 @@ import { Button as CommonButton, ButtonProps } from "../../_common/button";
 import styles from "./styles.module.scss";
 
 export function Button({ className, ...otherProps }: ButtonProps) {
-  return null;
+  return (
+    <CommonButton {...otherProps} className={`${styles.button} ${className}`} />
+  );
 }
