02eef330a1fb942c7f322d4a030a91216499fb4d
Emilio Heinzmann <emiliosheinz@gmail.com>
2022-08-22 08:50:10 -0300

    refactor: :recycle: adding environment variables

--- apps/app-a/pages/index.tsx
+++ apps/app-a/pages/index.tsx
@@ -4,6 +4,7 @@ export default function App() {
   return (
     <div>
       <h1>APP A</h1>
+      <h2>{`Environment: ${process.env.NEXT_PUBLIC_ENVIRONMENT}`}</h2>
       <Button>{"Default Button"}</Button>
     </div>
   );
--- apps/app-b/pages/index.tsx
+++ apps/app-b/pages/index.tsx
@@ -4,6 +4,7 @@ export default function App() {
   return (
     <div>
       <h1>APP B</h1>
+      <h2>{`Environment: ${process.env.NEXT_PUBLIC_ENVIRONMENT}`}</h2>
       <Button>{"Custom Button"}</Button>
     </div>
   );
